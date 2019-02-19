cfdf135a350f1cc573c0592c251e06496a0bef08
Snickers97 <jacobhandley50@gmail.com>
2019-02-19 17:43:59 -0500

    Fixed stuff

--- pp_ps.cc
+++ pp_ps.cc
@@ -4,9 +4,11 @@
 
 using namespace std;
 
+void output(string str[8]);
+
 int main(int argc, char **argv){
     //Input stuff
-    cout<<"PID                    Command   State    %CPU       %MEM       VSZ       RSS       Core"<<endl;
+    cout<<"PID            Command         State    %CPU       %MEM       VSZ       RSS       Core"<<endl;
     long pid;
     struct dirent *ep;
     DIR *dp;
@@ -17,11 +19,21 @@ int main(int argc, char **argv){
             if( ( ep->d_type == DT_DIR ) && ( pid > 0) ){
                 //printf("directory name: %s\n", ep->d_name);
                 // Do something with the proc directory
-                //string statfile = 
                 ifstream in;
-                in.open("/proc/"+pid+"/stat");
+                in.open("/proc/" + string(ep->d_name) + "/stat");
+                if(!in){
+                    cout<<"Error: unable to open stat file\n";
+                    exit(-1);
+                }
+                string str[8];
+                for(int i = 0; i < 8; i++){
+                    in>>str[i];
+                }
+                output(str);
             }
             //closedir(dp);
+            //This line gave me trouble so I commented it out
+            //The program works fine without it for now
         }
     }
     else{
@@ -29,4 +41,12 @@ int main(int argc, char **argv){
         exit(-1);
     }
     return 0;
+}
+
+void output(string str[8]){
+    cout<<str[0];
+    for(int i = 0; i < 14-str[0].size(); i++){
+        cout<<" ";
+    }
+    cout<<str[1]<<endl;
 }
