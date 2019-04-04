84024cb08f7b82de870c7c2935021997ec03b0d9
Jacob <jacobhandley50@gmail.com>
2019-04-03 23:27:28 -0400

    need the notebook

--- project2/cs_1.c
+++ project2/cs_1.c
@@ -16,18 +16,18 @@ typedef struct msg {
 int main(){
     int fd[2];
     pipe(fd);
-    struct msg message;
+    msg_t msg;
     int pid_c = fork();
     int pid_p = getpid();
     if(pid_c != 0){    //parent process
         while(1){
-            read(fd[0], message.message_text, MSG_LENGTH);
-            if(message.type == REGULAR){
+            read(fd[0], msg.message_text, MSG_LENGTH);
+            if(msg.type == REGULAR){
                 printf("server (PID:%d) received string:\n",pid_p);
-                printf(message.message_text);
+                printf(msg.message_text);
                 printf("from client (PID:%d)\n",pid_c);
             }
-            else if(message.type == COMMAND){
+            else if(msg.type == COMMAND){
                 printf("server (PID:%d) exits\n",pid_p);
                 break;
             }
@@ -39,17 +39,16 @@ int main(){
         printf("Enter a line of text\n");
         while(1){
             fgets(msgtype,6,stdin);
-            printf(msgtype);
-            printf("\n");
-            fgets(message.message_text,100,stdin);
+            fgets(msg.message_text,100,stdin);
             if(msgtype == "send"){
-                message.type = REGULAR;
+                msg.type = REGULAR;
             }
             else if(msgtype == "exit"){
-                message.type = COMMAND;
+                msg.type = COMMAND;
             }
-            write(fd[1], message.message_text, MSG_LENGTH);
-            if(message.type == COMMAND){
+            //printf("Type: %s\n",msg.type);
+            write(fd[1], msg.message_text, MSG_LENGTH);
+            if(msg.type == COMMAND){
                 break;
             }
         }
