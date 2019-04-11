10b27d6a1bef014d1b11b04aba6c2b5dcf26521c
Jacob <jacobhandley50@gmail.com>
2019-04-11 14:11:01 -0400

    Mostly done

--- project2/part2/cs_2_client.c
+++ project2/part2/cs_2_client.c
@@ -8,6 +8,8 @@ example on Blackboard*/
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_BUF 1024
 #define MSG_LENGTH 100
@@ -26,6 +28,8 @@ typedef struct msg {
 
 typedef struct initial_msg{
     char pid[MSG_LENGTH];
+    char writepipe[MSG_LENGTH];
+    char readpipe[MSG_LENGTH];
 } initial_msg_t;
 
 int main(){
@@ -36,14 +40,15 @@ int main(){
     sprintf(buf, "%d", getpid());
     strcpy(initial_msg.pid,buf);
     char writepipe[MAX_BUF] = "./";
-    strcat(writepipe,buf);
-    char tmp[MAX_BUF] = "_send";
-    strcat(writepipe,tmp);
+    sprintf(writepipe + strlen(writepipe),"%s",buf);
+    sprintf(writepipe + strlen(writepipe),"_send");
     char readpipe[MAX_BUF] = "./";
-    strcat(readpipe,buf);
-    char tmp2[MAX_BUF] = "_receive";
-    strcat(readpipe,tmp2);
+    sprintf(readpipe + strlen(readpipe),"%s",buf);
+    sprintf(readpipe + strlen(readpipe),"receive");
     char * myfifo = "./server_np";
+    strcpy(initial_msg.writepipe, writepipe);
+    strcpy(initial_msg.readpipe, readpipe);
+
 
     /* write PID to the FIFO */
     fd = open(myfifo, O_WRONLY);
@@ -51,6 +56,52 @@ int main(){
         fprintf(stderr, "Error\n");
 
     close(fd);
+    mkfifo(writepipe, 0660);
+    mkfifo(readpipe, 0660);
+    fd = open(writepipe, O_WRONLY);
+    ret = open(readpipe, O_RDONLY);
+    msg_t msg;
+    char msgtype[3];        //checks what type of message
+    printf("Enter a line of text\n");
+    printf("Type 'send: <message>' to send a message or 'exit' to exit.\n");
+    while(1){
+        fgets(msgtype,7,stdin);     //gets the message type, including colon and space
+        //send message
+        if(msgtype[0] == 's' && msgtype[1] == 'e' && msgtype[2] == 'n' && msgtype[3] == 'd'){
+            //I really hate C sometimes and would have preferred to use the C++ str.substr() method
+            //This was the easiest way I could think of to check this in C
+            msg.type = REGULAR;
+            fgets(msg.message_text,100,stdin);
+            write(fd, (char *) &msg, sizeof(msg));
+        }
+        //return current time to the client
+        else if(msgtype[0] == 't' && msgtype[1] == 'i' && msgtype[2] == 'm' && msgtype[3] == 'e'){
+            msg.type = TIME;
+            write(fd, (char *) &msg, sizeof(msg));
+            read(ret, (char *) &msg, sizeof(msg));
+            printf("\nCurrent time: %s\n",msg.message_text);
+        }
+        //return numner of clients to the client
+        else if(msgtype[0] == 's' && msgtype[1] == 't' && msgtype[2] == 'a' && msgtype[3] == 't'){
+            msg.type = STATUS;
+            write(fd, (char *) &msg, sizeof(msg));
+            read(ret, (char *) &msg, sizeof(msg));
+            printf("Number of clients: %s\n",msg.message_text);
+        }
+        //exit
+        else if(msgtype[0] == 'e' && msgtype[1] == 'x' && msgtype[2] == 'i' && msgtype[3] == 't'){
+            msg.type = COMMAND;
+            write(fd, (char *) &msg, sizeof(msg));
+            close(fd);
+            close(ret);
+            unlink(writepipe);
+            unlink(readpipe);
+            exit(0);
+        }
+        //User did not enter either send or exit:
+        else
+            continue;
+    }
 
     return 0;
 }
--- project2/part2/cs_2_server.c
+++ project2/part2/cs_2_server.c
@@ -6,12 +6,28 @@ example on Blackboard*/
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 #define MAX_BUF 1024
 #define MSG_LENGTH 100
 
+typedef enum{
+    REGULAR,
+    COMMAND,
+    TIME,
+    STATUS
+} msg_type_t;
+
+typedef struct msg {
+    msg_type_t type;
+    char message_text[MSG_LENGTH];
+} msg_t;
+
 typedef struct initial_msg{
     char pid[MSG_LENGTH];
+    char writepipe[MSG_LENGTH];
+    char readpipe[MSG_LENGTH];
 } initial_msg_t;
 
 int main(){
@@ -22,6 +38,7 @@ int main(){
     mkfifo(myfifo, 0660);
 
     initial_msg_t initial_msg;
+    int client_count = 0;
     /* open, read, and display the message from the FIFO */
     while(1){
         fd = open(myfifo, O_RDONLY);
@@ -31,8 +48,57 @@ int main(){
             printf("Received: %s\n", initial_msg.pid);
             is_fork = fork();
         }
-        if(is_fork == 0){
-            printf("Child server for %s\n",initial_msg.pid);
+        if(is_fork > 1){        //A fork has ocurred (from the parent's view)
+            client_count++;
+        }
+        if(is_fork == 0){       //Child server
+            client_count++;     //The client must count itself
+            close(fd);
+            int pid_p = getpid();
+            int ret;
+            fd = open(initial_msg.writepipe, O_RDONLY);
+            ret = open(initial_msg.readpipe, O_WRONLY);
+            while(1){
+                msg_t msg;
+                read(fd, (char *) &msg, sizeof(msg));
+                if(msg.type == REGULAR){
+                    printf("\nserver (PID:%d) received string:\n",pid_p);
+                    printf("%s",msg.message_text);
+                    printf("from client (PID:%s)\n\n",initial_msg.pid);
+                }
+                else if(msg.type == TIME){
+                    printf("server received time request from client\n");
+                    long sec,min,hr;
+                    //Find length in seconds of current day and adjust for timezone
+                    //This configuration assumes a machine in the US Eastern time zone,
+                    //not valid for other machines
+                    sec = (time(NULL) - 4*3600) % 86400;
+                    hr = (24*sec/86400);        //Convert total secs to hours and minutes
+                    sec = sec % 3600;
+                    min = (60*sec/3600);
+                    sec = sec % 60;
+                    char times[MSG_LENGTH];
+                    sprintf(times, "%lu",hr);       //Combine times into a string
+                    sprintf(times + strlen(times), ":");
+                    sprintf(times + strlen(times), "%lu",min);
+                    sprintf(times + strlen(times), ":");
+                    sprintf(times + strlen(times), "%lu",sec);
+                    //printf("Current time: %s\n",times);
+                    strcpy(msg.message_text,times);
+                    write(ret,(char *) &msg, sizeof(msg));
+                }
+                else if(msg.type == STATUS){
+                    sprintf(msg.message_text, "%d", client_count);
+                    printf("server received client count request\n");
+                    write(ret,(char *) &msg, sizeof(msg));
+                }
+                else if(msg.type == COMMAND){
+                    printf("server (PID:%d) exits\n",pid_p);
+                    close(fd);
+                    close(ret);
+                    _exit(0);
+                }
+            }
         }
 
         close(fd);
